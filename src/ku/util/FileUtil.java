ef203d6993b5f449b460bc8b78e402589f2f5210
Pittayut Sothanakul <pittayut.sot@ku.th>
2017-03-07 21:35:58 +0700

    modify code

--- src/ku/util/FileUtil.java
+++ src/ku/util/FileUtil.java
@@ -1,7 +1,13 @@
 package ku.util;
 
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+
 /*
  *   @author Pittayut Sothanakul
  */
@@ -9,13 +15,21 @@ public class FileUtil {
 
 	static void copy(InputStream in, OutputStream out) {
 		try {
-			int count = 0;
-			while (count > -1) {
-				count = in.read();
-				out.write(count);
+			int count ;
+			while ( (count =in.read()) != -1) {
+				out.write(count);	
 			}
-		} catch (Exception e) {
+		} catch (IOException e) {
 			throw new RuntimeException();
+		} 
+		finally {
+			try {
+				in.close();
+				out.close();
+
+			} catch (IOException e) {
+
+			}
 		}
 	}
 
@@ -24,17 +38,40 @@ public class FileUtil {
 		try {
 			byte[] buffer = new byte[blocksize];
 			int count = 0;
-			while (count != 1) {
+			while (count != -1) {
 				count = in.read(buffer);
-				out.write(count);
+				out.write(buffer);
 			}
 
 		} catch (Exception e) {
 			throw new RuntimeException();
+		} finally {
+			try {
+				in.close();
+				out.close();
+			} catch (IOException e) {
+
+			}
 		}
 	}
 
-	static void bcopy(InputStream inputStream, OutputStream out) {
+	static void bcopy(InputStream in, OutputStream out) throws RuntimeException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		PrintWriter printWriter = new PrintWriter(out);
+		String text;
+		try {
+			while ( (text = reader.readLine()) != null) {
+				printWriter.write(text);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException();
+		} finally {
+			try {
+				in.close();
+				out.close();
+			} catch (IOException e) {
 
+			}
+		}
 	}
 }
